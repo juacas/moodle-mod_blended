d86d2c0770ac86cb6c9c73756ea2b97133351058
luireg <luireg@95dcb3f6-ae72-456f-a756-099e62310317>
2015-05-13 21:21:59 +0000

    Nuevas sentencias

--- lang/en/blended.php
+++ lang/en/blended.php
@@ -2,5 +2,101 @@
 $string['modulename']       = 'Blended';
 $string['modulenameplural'] = 'Blendeds';
 
+//view.php
+$string['assignmentpage'] = 'Generate assignment page';
+$string['blendedquizzes'] = 'Blended Quizzes';
+$string['pluginadministration'] = 'Blended Configuration';
+$string['labelspage'] = 'Generate a page with stickers';
+$string['management'] = 'Team and assignment management';
+$string['studentOptions'] = 'Students\' Options';
+$string['gradepage'] = 'Grade assignmnent';
+$string['teamsmanagementpage'] = 'Manage teams';
+$string['signupteampage'] = 'Sign up team';
+$string['nostudentsincourse'] = 'Links disabled:<br><br>! There are not students in the course!';
+//Revisar estas dos variables...
+$string['noidnumberview'] = 'Student has not filled in DNI profile field!';
+$string['nouserinfodataview']  = 'Student has not filled in Personalized ID profile field !';
+$string['studentisnotactive'] = 'There are inactive students in the course!';
+$string['generatepaperquiz'] = 'Generate batch of questionnaires in paper';
+$string['scan'] = 'Process scanned quizzes';
+$string['correction'] = 'Supervise the state of the questionnaires';
+$string['revision'] = 'Test revision';
+$string['header1'] = 'HEADER1';
+$string['header2'] = 'HEADER2';
+$string['labelsGenerateStickersdesc'] = 'Print codebar stickers to identify the students. Stick them in the quizzes header in the ID field. <br>
+				Configure the number of labels to generate and choose the students of whom the labels will be generated. <p align=center></p> ';
+$string['managementGenerateTaskSheet'] = 'This tool can create templates for your face-to-face activities in your classroom.';
+$string['managementTeamGrading'] = 'Here you can grade the classroom tasks using the teams made for face-to-face work. The grades are inserted in the gradebook taking into account the configured teams.';
+$string['managementTeamCreation'] = 'Creation and management of the teams for presential tasks. The teams can be made by the teacher of by the students themselves.';
 
+
+//assignmentpage.php
+$string['assignmentpagedescr'] = 'Select the assignment page to print';
+$string['defaultassignment'] = 'Assignmnent: ';
+$string['assignments'] = 'Select activity: ';
+$string['assignment'] = 'Assignment (if not in the list): ';
+$string['user'] = 'Student: ';
+$string['noactiveuser'] = 'With (*) the students are not active in the course!';
+$string['noidnumber'] = 'With (**) the students have not introduced their DNI number !';
+$string['noidnumber2'] = 'Missing ID!';
+$string['nouserinfodata']  = 'With (#) the students that have not introduced their PERSONALIZED ID !';
+$string['noselected'] = 'Select: ';
+$string['printassignmentpage'] = 'Print assignment page';
+$string['noassignments'] = 'There are no assignments created in this course.';
+
+
+//grades.php
+$string['name'] = 'Assignment name';
+$string['duedate'] = 'Due date: ';
+$string['graded'] = 'Graded';
+$string['no'] = 'No';
+$string['yes'] = 'Yes';
+$string['partially'] = 'Partially';
+$string['numteams'] = 'Number of teams:';
+$string['teams'] = 'Number of teams';
+$string['resetteams'] = 'Reset teams';
+$string['createteams2'] = 'Create/Modify/Delete teams';
+$string['teamsmanagementpagedesc'] = 'Create teams for each assignment, manual or randomly.';
+$string['pagehelp'] = 'Page help contents';
+
+
+//introteams.php
+$string['introteams'] = 'Team management for ';
+$string['creationmethod'] = 'Team creation method: ';
+$string['byhand'] = 'Manual';
+$string['randomly'] = 'Random';
+$string['studentsselection'] = 'Students selection:';
+$string['activestudents'] = 'Only active';
+$string['allstudents'] = 'All';
+$string['nummembers'] = 'Number of members in a team:';
+$string['introgradepage'] = 'Insert grades';  
+$string['resetgroups'] = 'Reset or create a set og teams';
+
+
+//labels.php
+$string['labelspagedescr1'] = 'Select the student the number of labels by row and column that will be printed in a page';
+$string['labelspagedescr2'] = 'Select the number of labels by row and column that will be printed in a page';
+$string['numrows'] = 'Number of columns: ';
+$string['numcolumns'] = 'Number of rows: ';
+$string['printlabels'] = 'Print Labels';
+$string['pageformat'] = 'Page Format';
+$string['printforone'] = 'Print for one student.';
+$string['layoutmethod'] = 'Methods for layout labels';
+$string['oneforeachactive'] = 'One label for each active student in the course.';
+$string['oneforeachenrolled'] = 'One label for each student enrolled in the course.';
+$string['fullpages'] = 'Full pages of labels for the students selected in the list on the left.';
+$string['labelsformat'] = 'Course labels format';
+$string['identifyforhumans'] = 'Identify labels for humans';
+$string['donotidentify'] = 'Do not identify';
+$string['showreadableid'] = 'Show human-readable id';
+$string['showfullname'] = 'Show fullname';
+$string['margin_top_mm'] = '';
+$string['margin_top_mm'] = 'Margin Top mm (Measure carefully in your stickers sheet)';
+$string['margin_bottom_mm'] = 'Margin Bottom mm (Measure carefully in your stickers sheet)';
+$string['margin_right_mm'] = 'Margin Right mm (Measure carefully in your stickers sheet)';
+$string['margin_left_mm'] = 'Margin Left mm (Measure carefully in your stickers sheet)';
+
+
+$string['selectitems'] = 'Only show selected items. (All if none selected)';
+$string['selectitems_help'] = 'If you want to use Blended only for grading a number of items, you can select here which ones are activated.';
 ?>
