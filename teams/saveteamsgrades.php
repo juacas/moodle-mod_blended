6457218ed6e3f375aed33115e03b0299959205b9
juacas <juacas@95dcb3f6-ae72-456f-a756-099e62310317>
2015-05-15 10:59:55 +0000

    debugging unchanged grades in itastdevserver

--- teams/saveteamsgrades.php
+++ teams/saveteamsgrades.php
@@ -110,14 +110,14 @@ foreach ($teams as $team) {
             continue;
         }
         $rawgrade = $team->grade->grade == -1 ? null : $team->grade->grade;
-        $newfinalgrade = $team->grade->grade == -1 ? null : $item->adjust_raw_grade($team->grade->grade, $item->grademin, $item->grademax);
-        $outputmessages.="<p>New finalgrade $newfinalgrade for team $current_team->name</p>";
+        $newfinalgrade = (!isset($team->grade->grade) || $team->grade->grade == -1) ? null : $item->adjust_raw_grade($team->grade->grade, $item->grademin, $item->grademax);
+//        $outputmessages.="<p>New finalgrade $newfinalgrade for team $current_team->name</p>";
         blended_grade_team($item, $current_team, $newfinalgrade);
         foreach ($team->members as $memberid) {
 //                                if ($team->grade->rewrite)
             //Introduzco de nuevo las calificaciones
             $grade_prev = $item->get_grade($memberid);
- $outputmessages.="<p>User $memberid had grade $grade_prev->finalgrade</p>";
+// $outputmessages.="<p>User $memberid had grade $grade_prev->finalgrade</p>";
              if (!isset($grade_prev->finalgrade) && !isset($newfinalgrade)) { // skip students with no changes
                 continue;
             }
